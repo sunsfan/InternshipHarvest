3d1549d8c0f267337e6f5dc1d9b4fb01ca56a6e3
sunsfan <123209877@qq.com>
2016-12-07 14:38:26 +0800

    slf4j+logback commit

---
+++ LogTest/src/com/sunteng/test/start/Start.java
@@ -0,0 +1,15 @@
+package com.sunteng.test.start;
+
+import com.sunteng.test.a.TestA;
+import com.sunteng.test.b.TestB;
+import com.sunteng.test.c.TestC;
+import com.sunteng.test.d.TestD;
+
+public class Start {
+	public static void main(String[] args) {
+        TestA a = new TestA();
+        TestB b = new TestB();
+        TestC c = new TestC();
+        TestD d = new TestD();
+    }
+}
